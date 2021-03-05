799e0de29b327b8fb03435b230b4f6e00f9bec24
hangueira <hks713@naver.com>
2021-03-05 21:23:08 +0900

    js arrow-up event ADD

--- main.js
+++ main.js
@@ -39,7 +39,20 @@ document.addEventListener('scroll', () => {
   home.style.opacity = 1 - window.scrollY / homeHeight;
 })
 
+// show "arrow up" button
+const arrowUp = document.querySelector('.arrow-up');
+document.addEventListener('scroll', () => {
+  if(window.scrollY > homeHeight / 2) {
+    arrowUp.classList.add('visible');
+  } else {
+    arrowUp.classList.remove('visible');
+  }
+})
 
+// click on the "arrow up" button
+arrowUp.addEventListener('click', () => {
+  scrollIntoView('#home');
+ });
 
 // scroll function
 function scrollIntoView(selector) {
